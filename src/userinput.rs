62f240073f98386be1b8b87c9c806f390d6b37b8
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-03-04 21:29:32 -0500

    feat: user input is valididated for proper chars

--- src/userinput.rs
+++ src/userinput.rs
@@ -6,9 +6,44 @@ pub fn get_user_input() -> String {
 
     // Creating empty string to store the user input
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
-    println!("{}!", input.trim());
+    io::stdin().read_line(&mut input).unwrap();
+    input = input.trim().to_string();
+
+    while !validate_input(&input) {
+        println!("Invalid input, please try again");
+        input.clear();
+        io::stdin().read_line(&mut input);
+        input = input.trim().to_string();
+    }
+
 
     input
 
 }
+
+
+// Based on the input return true or false if it contains the right variables
+// Does not check if its a valid expression e.g. 3{{ would give true
+pub fn validate_input(input: &str) -> bool {
+
+    let valid_chars = ['+', '-', '*', '/', '(', ')', '.', ' ', '^', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    for letter in input.chars(){
+        // Used as indicator for if the current char is valid
+        let mut valid = false;
+        
+        for char in valid_chars{
+            if letter == char {
+                valid = true;
+                break;
+            }
+        }
+        // If the char is not valid return false
+        if !valid {
+            return false;
+        }
+    }
+    
+    // If all chars are valid return true
+    true
+}   
