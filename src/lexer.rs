0eb1dad247b346d18c41b6cc9ad8c012ea9369e5
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-03-05 14:48:06 -0500

    feat: lexer implemented for numbers and addition

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,44 @@
-// This file is used for taking the raw string from the user and converting it into a vector of tokens that can be used by the parser
+mod token;
 
+// This file is used for taking the raw string from the user and converting it into a vector of tokens that can be used by the parser
 // Goal is to return a vector of tokens
 
+// Tokenize function will return a vector of token enums (does not check if expression is valid)
+pub fn tokenize(input: &str) -> Result<Vec<Token>, String>{
 
-// Tokenize function will do multiple things, mainly to tokenize but also if invalid string, return error
-pub fn tokenize(&input) -> Result<Vec<>, String>{
+    // stack used for validating parentheses
+    let mut stack = Vec::new();
+
+    // buffer used for building multi digit numbers and decimals
+    let mut num_buffer = String::new();
+
+    // vector to store the tokens (the thing we will return at the end)
+    let mut tokens = Vec::new();  
+
+    let mut prev_char_is_num: bool = false;
+    
+    // Iterating through every character and creating enums for each
+    for c in input.chars(){
+        match c{
+            '0'..='9' | '.' => {
+                num_buffer.push(c);
+                prev_char_is_num = true;
+            }
+            '+' => {
+                if prev_char_is_num {
+                    tokens.push(TOKEN::NUMBER(num_buffer.parse().unwrap())) 
+                    num_buffer.clear(); // Emptying the buffer
+                    tokens.push(TOKEN::MULTIPLY)
+                }
+            }
+            '-'
+            '*'
+            '/'
+            '('
+            ')'
+            '^'
+            _ => Err(String::from("unknown char"))
+        }
+    }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod lexer;
 mod parser; 
 mod evaluator;
 mod userinput;
+mod token;
 
 use crate::userinput::get_user_input;
 
